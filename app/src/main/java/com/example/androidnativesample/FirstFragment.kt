5b3cd69c9dded9a3681892bd61d80ccd42ed69fa
huycozy <huy@nevercode.io>
2023-09-05 11:31:52 +0700

    108443
    Signed-off-by: Nguyen Huy <huy@nevercode.io>

--- app/src/main/java/com/example/androidnativesample/FirstFragment.kt
+++ app/src/main/java/com/example/androidnativesample/FirstFragment.kt
@@ -32,12 +32,6 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-            counter++
-            binding.textviewFirst.text = counter.toString()
-        }
     }
 
     override fun onDestroyView() {
